065ef016749f2d4895d4c555567fea67c296e841
lostsquirrel <songli1988@gmail.com>
2018-11-10 14:46:11 +0800

    add balance

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -11,26 +11,49 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = {
-      if (c == 0) 1
-      else {
-        if (c == r) 1
-        else
-          pascal(c -1, r - 1) + pascal(c, r - 1)
-      }
-
-
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0) 1
+    else {
+      if (c == r) 1
+      else
+        pascal(c - 1, r - 1) + pascal(c, r - 1)
     }
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+
+
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    var x = 0
+
+    def loop(charsx: List[Char]): Unit = {
+      if (!charsx.isEmpty) {
+        if (charsx.head == '(') x += 1
+        else if (charsx.head == ')')
+          if (x > 0) x -= 1
+          else {
+            x = -1
+            return
+          }
+        loop(charsx.tail: List[Char])
+      }
+    }
+
+    loop(chars)
+    x == 0
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    var res = 0
+    coins.sortWith(_ < _)
+    
+    res
+  }
+}
